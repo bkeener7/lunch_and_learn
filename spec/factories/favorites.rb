66ee662a52682c7cdbca7f2ae7c45fac3a0bf583
bkeener <bkeener7@gmail.com>
2023-01-18 06:11:31 -0700

    Re-update favorite FactoryBot

--- spec/factories/favorites.rb
+++ spec/factories/favorites.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :favorite do
     country { Faker::Address.country }
-    recipe_link { Faker::Address.country }
-    recipe_title { Faker::Address.country }
+    recipe_link { Faker::Internet.url }
+    recipe_title { Faker::Food.dish }
     association :user, factory: :user
   end
 end
