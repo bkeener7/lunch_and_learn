cdaa6cbee3ffa5747c94a7569b90de5704133636
bkeener <bkeener7@gmail.com>
2023-01-18 06:53:17 -0700

    Refactor create favorites controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -10,15 +10,15 @@ class Api::V1::FavoritesController < ApplicationController
   end
 
   def create
-    if @user.nil?
-      render json: { message: 'API key not valid' }, status: :bad_request
-    else
+    if @user
       new_favorite = @user.favorites.new(favorite_params)
       if new_favorite.save
         render json: { "success": 'Favorite added successfully' }, status: :created
       else
         render json: { message: new_favorite.errors.full_messages.to_sentence }, status: :bad_request
       end
+    else
+      render json: { message: 'API key not valid' }, status: :bad_request
     end
   end
 end
